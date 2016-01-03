032cf817804ad31ef6e8e631501939f4f70dc0ad
jmangel <john.m5321@yahoo.com>
2016-01-02 17:44:23 -0800

    Adds methods for permissions and projects
    Should fix issue #10

--- dlx-api.js
+++ dlx-api.js
@@ -6,23 +6,21 @@ var dlxApi = {
   
   deleteBundles (ids) {},
   
-  getBundle (id) {},
-  
-  getBundles (ids) {},
-  
-  upsertBundle (bundle) {},
-  
-  upsertBundles (bundles) {},
-  
-  /*item methods*/
-
   deleteItemFromBundle (id, bundleID) {},
   
   deleteItemsFromBundle (ids, bundleID) {},
   
+  getBundle (id) {},
+  
+  getBundles (ids) {},
+  
   getItemFromBundle (id, bundleID) {},
   
   getItemsFromBundle (ids, bundleID) {},
+  
+  upsertBundle (bundle) {},
+  
+  upsertBundles (bundles) {},
   
   upsertItemsToBundle (items, bundleID) {},
   
@@ -30,36 +28,95 @@ var dlxApi = {
   
   /*lexicon methods*/
   
-  deleteLexicon (id) {},
-  
-  getLexicon (id) {},
-  
-  upsertLexicon (lexi) {},
-  
-  /*lexicon entry methods*/
-
   deleteEntriesFromLexicon (ids, lexiID) {},
   
   deleteEntryFromLexicon (id, lexiID) {},
+  
+  deleteLexicon (id) {},
   
   getEntriesFromLexicon (ids, lexiID) {},
   
   getEntryFromLexicon (id, lexiID) {},
   
+  getLexicon (id) {},
+  
   upsertEntriesToLexicon (entries, lexiID) {},
   
   upsertEntryToLexicon (entry, lexiID) {},
   
+  upsertLexicon (lexi) {},
+  
   /*permission methods*/
   
-  upsertPermission (permission, user) {}, /* not sure what exactly is meant by permissions and this example method,
-    I assume it will need these two parameters*/
+  addPermission (user, resource, permissionType) {},
   
-  /*phrase methods*/
+  addPermissions (users, resources, permissionTypes) {}, /*should this be 
+    permissionType or permissionTypes? Based on your comments it says singular,
+    but I think you meant plural? */
 
-  getPhraseFromText (id, textID) {},
+  authenticate () {},
   
-  getPhrasesFromText (ids, textID) {},
+  deletePermission (user, resource, permissionType) {},
+  
+  deletePermissions (users, resources, permissionType) {},
+  
+  makePrivate (resourceID, revokeExistingPermissions=false) {},
+  
+  makePublic (resourceID),
+  
+  /*project methods*/
+  
+  addBundleToProject (bundID, projID) {},
+  
+  addBundlesToProject (bundIDs, projID) {},
+  
+  addLanguageToProject (langID, projID) {},
+  
+  addLanguagesToProject (langIDs, projID) {},
+  
+  addLexiconToProject (lexiID, projID) {},
+  
+  addLexiconsToProject (lexiIDs, projID) {},
+  
+  addLocationToProject (locaID, projID) {},
+  
+  addLocationsToProject (locaIDs, projID) {},
+  
+  addMediaToProject (mediaID, projID) {},
+  
+  addMediasToProject (mediaIDs, projID) {},
+  
+  addTextToProject (textID, projID) {},
+  
+  addTextsToProject (textIDs, projID) {},
+  
+  removeBundleFromProject (bundID, projID) {},
+  
+  removeBundlesFromProject (bundIDs, projID) {},
+  
+  removeLanguageFromProject (langID, projID) {},
+  
+  removeLanguagesFromProject (langIDs, projID) {},
+  
+  removeLexiconFromProject (lexiID, projID) {},
+  
+  removeLexiconsFromProject (lexiIDs, projID) {},
+  
+  removeLocationFromProject (locaID, projID) {},
+  
+  removeLocationsFromProject (locaIDs, projID) {},
+  
+  removeMediaFromProject (mediaID, projID) {},
+  
+  removeMediasFromProject (mediaIDs, projID) {},
+  
+  removeTextFromProject (textID, projID) {},
+  
+  removeTextsFromProject (textIDs, projID) {},
+  
+  /*properties*/
+  
+  accessToken: "",
   
   /*text methods*/
 
@@ -67,9 +124,13 @@ var dlxApi = {
   
   deleteTexts (ids) {},
   
+  getPhraseFromText (id, textID) {},
+  
+  getPhrasesFromText (ids, textID) {},
+  
   getText (id) {},
   
-  getTexts (ids) {}
+  getTexts (ids) {},
 
   upsertText (text) {},
   
